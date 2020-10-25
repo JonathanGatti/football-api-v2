61453138cd900060efb2ca38106096e85c6a612d
jonathan gatti <jonathangatti2@gmail.com>
2020-10-25 15:56:02 +0000

    added app js

--- public/app.js
+++ public/app.js
@@ -6,10 +6,6 @@ const getPlayer = document.querySelector('#get-player');
 const list = document.querySelector('#list');
 
 
-const config = {
-  api_key: '89f56fc814mshaffde0d4f2ea23cp144e32jsnf8dcdcfa56ff'
-}
-
 const team = {
   teamName: {
     name: ''
