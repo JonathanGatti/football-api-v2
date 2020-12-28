d1feb05c8ed69ecc85f6d2e6f6a4b32ce6db164b
jonathan gatti <jonathangatti2@gmail.com>
2020-12-28 10:35:34 +0000

    env file and connected to mongoatlas

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const  mongoose = require("mongoose");
 mongoose.set('debug', true);
 const url = 'mongodb://127.0.0.1:27017/football-app';
+dbUrl = process.env.DB_URL
 
-mongoose.connect(url,  {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.Promise = Promise;
 
